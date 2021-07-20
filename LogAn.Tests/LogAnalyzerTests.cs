388beb617f641268f81d1f92447b8935eed99d2c
Jackie <giacatkhongminh93@gmail.com>
2021-07-20 22:42:36 +0700

    Create the first unit test

---
+++ LogAn.Tests/LogAnalyzerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn.Tests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void isValidLogFileName_BadExtension_ReturnFalse()
+        {
+            //Arrange
+            LogAnalyzer analyzer = new LogAnalyzer();
+            //Act
+            var result = analyzer.isValidLogFileName("someFile.exe");
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool isValidLogFileName(string fileName)
+        {
+            /*if (!File.Exists(fileName))
+                throw new Exception("No log file with that name exists");
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentException("Filename must to be provided");*/
+            if (!fileName.ToLower().EndsWith(".slf"))
+                return false;
+            return true;
+        }
+    }
+}

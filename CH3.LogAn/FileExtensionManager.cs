78044b3ebd82418f4501d5e69c476048bdb1a75c
Jackie <giacatkhongminh93@gmail.com>
2021-07-22 09:08:56 +0700

    Exception Stub

---
+++ CH3.LogAn/FileExtensionManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH3.LogAn
+{
+    public class FileExtensionManager : IExtensionManager
+    {    
+        public bool isValidLogFileName(string fileName)
+        {
+            if (!fileName.ToLower().EndsWith(".slf"))
+                return false;
+            return true;
+        }
+    }
+}

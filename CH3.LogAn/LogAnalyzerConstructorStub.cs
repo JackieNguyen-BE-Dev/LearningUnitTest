944e6333eb6db7d662c992a120b215769b7e83d7
Jackie <giacatkhongminh93@gmail.com>
2021-07-22 11:15:27 +0700

    Using Exception Stub

---
+++ CH3.LogAn/LogAnalyzerConstructorStub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH3.LogAn
+{
+    public class LogAnalyzerConstructorStub
+    {
+        private IExtensionManager _manager;
+
+        public LogAnalyzerConstructorStub(IExtensionManager manager)
+        {
+            _manager = manager;
+        }
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            return _manager.isValidLogFileName(fileName);
+        }
+
+    }
+}

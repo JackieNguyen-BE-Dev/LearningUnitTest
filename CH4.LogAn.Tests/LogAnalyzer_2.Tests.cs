7a8d7e90ae5ae79b8c3b24e9bb311305aa60d08d
Jackie <giacatkhongminh93@gmail.com>
2021-07-22 17:11:34 +0700

    Using Mock Object

---
+++ CH4.LogAn.Tests/LogAnalyzer_2.Tests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH4.LogAn.Tests
+{
+    [TestFixture]
+    public class LogAnalyzer_2Test
+    {
+        [Test]
+        public void Analyze_WebServiceThrows_SendsEmail()
+        {
+            StubService stubService = new StubService();
+            stubService.willThrow = new Exception("Fake exception");
+
+
+            MockService mockService = new MockService();
+
+            LogAnalyzer_2 analyzer_2 = new LogAnalyzer_2(stubService, mockService);
+            analyzer_2.Analyze("someFile.exe");
+
+
+            Assert.AreEqual("longindusrobot@gmail.com", mockService.To);
+            Assert.AreEqual("fake exception", mockService.Body);
+            Assert.AreEqual("can't log", mockService.Subject);
+        }
+    }
+
+
+    public class StubService : IWebService
+    {
+        public Exception willThrow;
+
+
+        public void LogError(string message)
+        {
+            if (willThrow != null)
+                throw willThrow;
+        }
+    }
+
+
+    public class MockService : IEmailService
+    {
+        public string To;
+
+        public string Subject;
+
+        public string Body;
+
+
+        public void SendEmail(string to, string subject, string body)
+        {
+            To = to;
+            Subject = subject;
+            Body = body;
+        }
+    }
+}
---
+++ CH4.LogAn/LogAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH4.LogAn
+{
+    public class LogAnalyzer
+    {
+        private readonly IWebService _webService;
+
+        public LogAnalyzer(IWebService webService)
+        {
+            _webService = webService;
+        }
+
+        [Conditional ("DEBUG")]
+        public void Anylize(string fileName)
+        {
+            if (fileName.Length < 8)
+                _webService.LogError("File name too short:" + fileName);
+        }
+    }
+}
